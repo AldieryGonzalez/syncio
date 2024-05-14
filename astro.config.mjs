ee8264a6fe80d363215abb4efbc9e4e9aaaadae2
Aldiery Gonzalez <aldieryrgon@gmail.com>
2024-05-13 23:12:20 -0500

    Feat: Added Extra tooling
    Tailwind, SolidJS, CloudFlare Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import solidJs from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [solidJs(), tailwind()],
+  output: "server",
+  adapter: cloudflare()
+});
